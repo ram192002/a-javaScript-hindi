742ded8c68dbb52ae21951624dfbb9763756fd6c
ram192002 <bcagkp63@gmail.com>
2024-08-22 20:26:36 +0530

    Add files via upload

---
+++ 2_datatype.js
@@ -0,0 +1,32 @@
+"user strict";
+
+// alert("hello") // 
+console.log(3+3);
+console.log(3-3);
+
+console.log("Ram");
+
+let name ="ram";  // string
+let age = 23;  // number
+let isMarried = false;  // boolean
+let city = null;  // object
+let country = undefined;
+
+
+// number  => 2 to power 54;
+// bigint
+// string
+// boolean  => true/false
+// null  => standalone value;
+// undefined => 
+    //symbol => unique 
+
+//object => 
+
+    console.log(typeof "ram");
+    console.log(typeof age);
+    console.log(typeof null); // object
+
+
+
+console.table([name ,age ,isMarried, city ,country])
