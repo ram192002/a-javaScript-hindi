91d84f047e7320c9119637127e85966032d95e74
ram192002 <bcagkp63@gmail.com>
2024-08-22 20:54:24 +0530

    Add files via upload

---
+++ 3_conversion.js
@@ -0,0 +1,68 @@
+
+let score =33;
+let Score = "33abc" 
+                                            // //  Notes
+  /* if Score of value =  null  then output is 0
+  if Score = undefine then output is  NaN
+  if boolean value 1 then output is 1
+  if boolean value 0 then output is 0
+
+  if Score of value ="string " then output is NaN
+
+  if Score = "33abc" then output is 33
+  if Score = "33" then output is 33
+  if Score = 33 then output is 33
+  if Score = 33abc then output is 33
+
+
+  */
+ console.log(typeof score);
+ console.log(typeof(score));
+
+ console.log(typeof Score)
+ console.log(typeof(Score))
+ //  score = String(score);
+
+ // value of " 33abc string and get output  typeof number"
+ let valueIntNumber = Number(Score);
+ console.log(typeof valueIntNumber)
+ console.log(valueIntNumber)
+
+
+
+
+
+  // boolean conversion....
+
+   let isLoggedIn =1;
+
+   console.log(typeof isLoggedIn)  ; // number
+   console.log(isLoggedIn);       // 1
+
+   let booleanIsLoggedIn = Boolean(isLoggedIn);
+   console.log(booleanIsLoggedIn); // true
+
+  /*
+ 1 => true ;
+ " " => false;
+ 0 => false;
+ null => false;
+ undefined => false;
+ NaN => false;
+ "ram" = true;
+ "0" = true;
+ "false" = true;
+ "true" = true;
+
+  */
+
+console.log("\n")
+
+ // number convert to sting 
+
+
+ let someNumber = 312;
+ let stringNumber = string(someNumber);
+  console.log(stringNumber);
+ console.log(typeof someNumber);
+
